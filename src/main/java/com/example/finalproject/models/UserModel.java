096c4fb695d78dfcdac4f7f2bce9f170b032fb2f
vhoang <nvh18102002@gmail.com>
2023-01-08 23:26:36 +0700

    Chinh sửa lại hàm

--- src/main/java/com/example/finalproject/models/UserModel.java
+++ src/main/java/com/example/finalproject/models/UserModel.java
@@ -8,17 +8,22 @@ import java.util.List;
 
 public class UserModel {
     public static void add(User c){
-        String insertSql = "insert into web_final.users (username, password, name, role, second_name, dob, email)\n" +
-                "values (:username,:password,:name,:role,:second_name,:dob,:email);";
+        String insertSql = "insert into web_final.users (username, password, name, issue_at, expiration, role, second_name, dob, email,\n" +
+                "                             otp, otp_exp)\n" +
+                "values (:username,:password,:name,:issue_at,:expiration,:role,:second_name,:dob,:email,:otp,:otp_exp)";
         try (Connection con = DbUtils.getConnection()) {
             con.createQuery(insertSql)
                     .addParameter("username", c.getUsername())
                     .addParameter("password", c.getPassword())
                     .addParameter("name", c.getName())
+                    .addParameter("issue_at", c.getIssue_at())
+                    .addParameter("expiration", c.getExpiration())
                     .addParameter("role", c.getRole())
                     .addParameter("second_name", c.getSecond_name())
                     .addParameter("dob", c.getDob())
                     .addParameter("email", c.getEmail())
+                    .addParameter("otp", c.getOtp())
+                    .addParameter("otp_exp", c.getOtp())
                     .executeUpdate();
         }
     }
