93311e148d77816ce93f0fa25809a55caa859c64
vhoang <nvh18102002@gmail.com>
2023-01-10 23:30:56 +0700

    Chỉnh sửa Home/Index

--- src/main/java/com/example/finalproject/controller/HomeServlet.java
+++ src/main/java/com/example/finalproject/controller/HomeServlet.java
@@ -1,7 +1,9 @@
 package com.example.finalproject.controller;
 
 import com.example.finalproject.beans.Articles;
+import com.example.finalproject.beans.Category;
 import com.example.finalproject.service.ArticleService;
+import com.example.finalproject.service.CategoryService;
 import com.example.finalproject.utils.ServletUtils;
 
 import javax.servlet.ServletException;
@@ -20,11 +22,16 @@ public class HomeServlet extends HttpServlet {
         if (path == null || path.equals("/")) {
             path = "/Index";
         }
-
         switch (path) {
             case "/Index":
-                List<Articles> list = ArticleService.getTop3();
-                request.setAttribute("article", list);
+                List<Articles> listtop3 = ArticleService.getTop3InWeek();
+                List<Articles> listtop10view = ArticleService.getTop10();
+                List<Articles> listtop10new = ArticleService.getTop10new();
+                List<Category> listAllCate = CategoryService.getAll();
+                request.setAttribute("listtop10new", listtop10new);
+                request.setAttribute("listtop3", listtop3);
+                request.setAttribute("listtop10view", listtop10view);
+                request.setAttribute("listAllCate", listAllCate);
                 ServletUtils.forward("/views/vwHome/Index.jsp", request, response);
                 break;
             case "/Contact":
